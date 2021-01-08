1d31bba9698896112499ca9d6115389684f16ca2
Ilya Golovin <60024405+IlyaGolovin@users.noreply.github.com>
2021-01-08 12:07:13 -0500

    First move with GIT

---
+++ PythonFiles/Slicing.py
@@ -0,0 +1,13 @@
+familyMembers1 = ["John", 'Peter',"Nata","Go","Rebeka","Lese","Mike","Henry","Bob",
+                  "Lana","Dan","Tom","Kate","Moere","Baku","xen","Kale"]
+print(familyMembers1[0:10])
+print(familyMembers1[3:13])
+print(familyMembers1[:-9])
+print(familyMembers1[0:10])
+print(familyMembers1[0:10])
+print(familyMembers1[0:10])
+print(familyMembers1[0:10])
+print(familyMembers1[0:10])
+print(familyMembers1[0:10])
+
+
